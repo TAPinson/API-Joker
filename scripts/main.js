63aeeac09a463c3dc3bbce8095335f95f3261705
Travis Pinson <travispinson@gmail.com>
2020-08-28 11:12:27 -0500

    completed project, verified OK by Adam

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,47 @@
-const jokeBtn = document.querySelector(".request-joke-btn");
+import { retrieveJoke } from './JokeProvider.js';
 
-jokeBtn.addEventListener("click", (e) => {
-  console.log("I should be grabbing a joke now...");
-  // TODO: fetch a joke from the Joke API and render it to the DOM
+let jokeData = [];
+
+const jokeBtn = document.querySelector(".request-joke-btn");
+const punchlineBtn = document.querySelector(".request-punchline-btn");
+const setupElement = document.querySelector(".joke-setup");
+const punchlineElement = document.querySelector(".joke-punchline")
+
+
+jokeBtn.addEventListener("click", () => {
+  retrieveJoke()
+	.then((responseData) => {
+		jokeData = responseData;
+    setupWriter(jokeData)
+	})
 });
+
+
+punchlineBtn.addEventListener("click", () => {
+  punchlineWriter(jokeData)
+})
+
+
+const setupWriter = (joke) => {
+  setupElement.innerHTML = ""
+  punchlineElement.innerHTML = ""
+  setupElement.innerHTML += `
+  ${joke.setup}`
+}
+
+
+const punchlineWriter = (joke) => {
+  punchlineElement.innerHTML = ""
+  punchlineElement.innerHTML += `
+  ${joke.punchline}`
+}
+
+
+
+
+
+
+
+
+
+
